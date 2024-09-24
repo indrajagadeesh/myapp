96f215a6c1bbcbd16cdf916d083d8da7ad48c5e1
indrajagadeesh121 <indrajagadeesh121@gmail.com>
2024-09-24 03:14:36 +0000

    Android

--- lib/screens/task_detail_screen.dart
+++ lib/screens/task_detail_screen.dart
@@ -2,7 +2,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../models/task.dart';
 import '../providers/task_provider.dart';
 import '../widgets/subtask_list.dart';
 import '../widgets/stopwatch_widget.dart';
